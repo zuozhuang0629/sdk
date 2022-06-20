34d6e1fa9044e54e7b15302469ec685ad8a21a10
zuoz <1071959323@qq.com>
2022-06-20 22:54:18 +0800

    初次提交

---
+++ sdk/src/main/java/com/facebook/sdk/SdkDialog.kt
@@ -0,0 +1,26 @@
+package com.facebook.sdk
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+
+/**
+ * create by zuoz
+ * time:2022/6/20
+ **/
+class SdkDialog : DialogFragment() {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
